552ea94906e27dc3d65359e4bcc7fa1ef175b1f9
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-05 19:58:49 +0300

    Add counter class with static count filed

---
+++ Exemple/ClassAndObject/Counter.cs
@@ -0,0 +1,16 @@
+﻿namespace Exemple.ClassAndObject
+{
+    public class Counter
+    {
+        private static int _count = 0;
+        public Counter()
+        {
+            _count++;
+        }
+
+        public void DisplayCount()
+        {
+            Console.WriteLine($"Count is: {_count}");
+        }
+    }
+}
--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Exemple.ClassAndObject;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Exemple
 {
@@ -8,11 +9,20 @@ namespace Exemple
 
         static void Main(string[] args)
         {
-            Person p = new Person();
+            /*Person p = new Person();
             Person p2 = new Person("Nicu", 10);
 
             p.DisplayInfo();
-            p2.DisplayInfo();
+            p2.DisplayInfo();*/
+
+            /*var counter1 = new Counter();
+            counter1.DisplayCount();
+            var counter2 = new Counter();
+            counter1.DisplayCount();
+            var counter3 = new Counter();
+            counter3.DisplayCount();
+            var counter4 = new Counter();
+            counter2.DisplayCount();*/
         }
     }
 }
