c5aa9cb2e7278eeddd7127714de899e51544767e
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-05 19:52:03 +0300

    Move verson to separate folder

---
+++ Exemple/ClassAndObject/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Exemple.ClassAndObject
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+        public Person()
+        {
+            Name = "Nume invalid";
+            Age = -1;
+        }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Numele persoanei este: {Name}, Varsta este: {Age}");
+        }
+    }
+}
--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -1,28 +1,10 @@
-﻿namespace Exemple
+﻿using Exemple.ClassAndObject;
+
+namespace Exemple
 {
     internal class Program
     {
-        public class Person
-        {
-            public string Name;
-            public int Age;
-
-            public Person()
-            {
-                Name = "Nume invalid";
-                Age = -1;
-            }
-            public Person(string name, int age)
-            {
-                Name = name;
-                Age = age;
-            }
-
-            public void DisplayInfo()
-            {
-                Console.WriteLine($"Numele persoanei este: {Name}, Varsta este: {Age}");
-            }
-        }
+        
 
         static void Main(string[] args)
         {
