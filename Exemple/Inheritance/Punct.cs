5110628973cb1230d45bdce90a3261b0dcf67aef
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-06 14:47:19 +0300

    Add Inheritance Example

---
+++ Exemple/Inheritance/Punct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemple.Inheritance
+{
+    public class Punct
+    {
+        protected int x;
+        protected int y;
+        public void Scrie()
+        {
+            Console.WriteLine("Punct: {0}, {1}", x, y);
+        }
+    }
+
+    public class Linie : Punct
+    {
+        public int xEnd;
+        public int yEnd;
+        public new void Scrie()
+        {
+            x = 2; y = 2;//acces la membrii din clasa de baza
+            Console.WriteLine("Segement: ({0}, {1}) - ({2}, {3})", x, y, xEnd, yEnd);
+        }
+    }
+
+}
--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Exemple.ClassAndObject;
+using Exemple.Inheritance;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Exemple
@@ -9,55 +10,13 @@ namespace Exemple
 
         static void Main(string[] args)
         {
-            /*Person p = new Person();
-            Person p2 = new Person("Nicu", 10);
-
-            p.DisplayInfo();
-            p2.DisplayInfo();*/
-
-            /*var counter1 = new Counter();
-            counter1.DisplayCount();
-            var counter2 = new Counter();
-            counter1.DisplayCount();
-            var counter3 = new Counter();
-            counter3.DisplayCount();
-            var counter4 = new Counter();
-            counter2.DisplayCount();*/
-
-            /*Console.WriteLine($"10 + 21 = {Calculator.Add(10, 21)}");
-            Console.WriteLine($"10 - 21 = {Calculator.Subtract(10, 21)}");
-            Console.WriteLine($"10 * 21 = {Calculator.Multiply(10, 21)}");
-            Console.WriteLine($"10 / 21 = {Calculator.Devide(10, 21)}");*/
-
-            /*var resourse = new Resource("test path");
-            resourse.UseResource();
-            resourse.UseResource();
-            resourse.UseResource();*/
-
-            /*var manager = new EmployeeManager();
-            manager.AddIntern("Nicu", 21);
-            manager.AddDeveloper("Vasile", 33);
-            manager.AddAccountant("Ina", 24);
-            manager.AddDesigner("Irina", 27);
-
-            manager.DisplayEmployeesUnder25();
-            foreach(var developer in manager.GetDevelopers())
-            {
-                Console.WriteLine(developer.Name);
-            }*/
-
-            var inventory = new Inverntory();
-            try
-            {
-                var product = inventory[0];
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine("Elementul este inexistent!");
-            }
-            inventory.AddProduct(new Product("telefon", 0.2));
-            var productExistent = inventory[0];
-            Console.WriteLine(productExistent.GetProductName());
+            Punct punct = new Punct();
+            //punct.x = 0; punct.y = 0; acces restrictionat
+            punct.Scrie();
+            Linie linie = new Linie();
+            //linie.x = 0; linie.y = 0;acces restrictionat
+            linie.xEnd = 10; linie.yEnd = 10;
+            linie.Scrie();
         }
     }
 }
