e441b85c40f816329f553afef8d8e56c243c0e0c
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-08 19:40:53 +0300

    Add vehicle Class

---
+++ Exemple/OOP/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿namespace Exemple.OOP
+{
+    public class Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class Car : Vehicle
+    {
+        public int NumberOfDoors { get; set; }
+    }
+
+    public class Truck : Vehicle
+    {
+        public int CargoCapity { get; set; }
+    }
+}
