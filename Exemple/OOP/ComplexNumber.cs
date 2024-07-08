a00f4b63f62a826f4d379f5576ac1b3a7e73187e
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-08 20:09:44 +0300

    Add ComplexNumber class

---
+++ Exemple/OOP/ComplexNumber.cs
@@ -0,0 +1,42 @@
+﻿namespace Exemple.OOP
+{
+    public class ComplexNumber
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.Real * b.Real - a.Imaginary * b.Imaginary,
+                a.Real * b.Imaginary + a.Imaginary * b.Real);
+        }
+
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            var denominator = b.Real * b.Real + b.Imaginary * b.Imaginary;
+            return new ComplexNumber((a.Real * b.Real + a.Imaginary * b.Imaginary) / denominator,
+                (a.Imaginary * b.Real - a.Real * b.Imaginary) / denominator);
+        }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imaginary}i";
+        }
+    }
+}
--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -8,9 +8,12 @@ namespace Exemple
 
         static void Main(string[] args)
         {
-            var operations = new MathOperations();
-            var result1 = operations.Add(1, 2);
-            var result2 = operations.Add(1.1, 2.2);
+            var number1 = new ComplexNumber(1, 2);
+            var number2 = new ComplexNumber(3, 4);
+
+            Console.WriteLine(number1 + number2);
+            Console.WriteLine(number1 - number2);
+            Console.WriteLine(number1 * number2);
         }
     }
 }
