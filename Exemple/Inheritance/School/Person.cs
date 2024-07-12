3e0af9481600ac8ec59e68b784e6675db8526ff3
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-12 19:59:03 +0300

    Add person class

---
+++ Exemple/Inheritance/School/Person.cs
@@ -0,0 +1,49 @@
+﻿namespace Exemple.Inheritance.School
+{
+    public class Person
+    {
+        internal string Name { get; set; }
+        public int Age { get; set; }
+
+        protected Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return  "Name: " + Name + ", Age: " + Age;
+        }
+    }
+
+    public class Student : Person
+    {
+        public int Grade { get; set; }
+
+        public Student(string name, int age, int grade) : base(name, age)
+        {
+            Grade = grade;
+        }
+
+        public new string ToString()
+        {
+            return base.ToString() + ", Grade: " + Grade;
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public string Subject { get; set; }
+
+        public Teacher(string name, int age, string subject) : base(name, age)
+        {
+            Subject = subject;
+        }
+
+        public new string ToString()
+        {
+            return base.ToString() + ", Subject: " + Subject;
+        }
+    }   
+}
--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-using System.Security.Cryptography;
+using Exemple.Inheritance.School;
 
 namespace Exemple
 {
@@ -9,37 +9,11 @@ namespace Exemple
 
         static void Main(string[] args)
         {
-            int Rand()
-            {
-                var rand = new Random();
-                return rand.Next(1, 200);
-            }
+            object studen = new Student("ion", 20, 5);
+            Person teacher = new Teacher("gigel", 30, "math");
 
-            int TrueRandom()
-            {
-                RandomNumberGenerator rng = new RNGCryptoServiceProvider();
-                try
-                {
-                    
-                    byte[] bytes = new byte[4];
-                    rng.GetBytes(bytes);
-                    rng.Dispose();
-                    return BitConverter.ToInt32(bytes, 0);
-                }
-                catch (CryptographicException)
-                {
-                    return Rand();
-                }
-                finally
-                {
-                    rng.Dispose();
-                }
-            }
-
-            for (int i = 0, j = 0; i < 100; i++, j++)
-            {
-                Console.WriteLine(TrueRandom());
-            }
+            Console.WriteLine(studen);
+            Console.WriteLine(teacher);
 
         }
     }
