89592ea87828634693ab649f9987fc633853b3c4
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-12 19:44:43 +0300

    Add class animal

---
+++ Exemple/Inheritance/Animal/Animal.cs
@@ -0,0 +1,40 @@
+﻿namespace Exemple.Inheritance.Animal
+{
+    public class Animal : IAnimal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+
+    public class Cat : Animal, ICat
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Cat is meowing");
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Cat is playing");
+        }
+    }
+
+    public class Bird : Animal, IBird
+    {
+        public new void Eat()
+        {
+            Console.WriteLine("Bird is eating");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Bird is flying");
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exemple/Inheritance/Animal/IAnimal.cs
@@ -0,0 +1,22 @@
+﻿namespace Exemple.Inheritance.Animal
+{
+    public interface IAnimal
+    {
+        void Eat();
+    }
+
+    public interface IPet
+    {
+       void Play();
+    }
+
+    public interface ICat : IAnimal, IPet
+    {
+        void Meow();
+    }
+
+    public interface IBird : IAnimal
+    {
+        void Fly();
+    }
+}
