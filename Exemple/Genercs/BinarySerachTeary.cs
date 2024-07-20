7661d8a72a81863386fa0c759950021e1555130f
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-20 11:21:10 +0300

    Add binary search tree

---
+++ Exemple/Genercs/BinarySerachTeary.cs
@@ -0,0 +1,92 @@
+﻿namespace Exemple.Genercs
+{
+    public class BinarySerachTeary<T>
+    {
+        private TreeNode<T> root;
+
+        public BinarySerachTeary()
+        {
+            root = null;
+        }
+
+        public void Insert(T value)
+        {
+            root = Insert(root, value);
+        }
+
+        private TreeNode<T> Insert(TreeNode<T> node, T value)
+        {
+            if (node == null)
+            {
+                node = new TreeNode<T>(value);
+            }
+            else
+            {
+                if (Comparer<T>.Default.Compare(value, node.Value) < 0)
+                {
+                    node.Left = Insert(node.Left, value);
+                }
+                else
+                {
+                    node.Right = Insert(node.Right, value);
+                }
+            }
+
+            return node;
+        }
+
+        public bool Search(T value)
+        {
+            return Search(root, value);
+        }
+
+        private bool Search(TreeNode<T> node, T value)
+        {
+            if (node == null)
+            {
+                return false;
+            }
+
+            if (Comparer<T>.Default.Compare(value, node.Value) == 0)
+            {
+                return true;
+            }
+
+            if (Comparer<T>.Default.Compare(value, node.Value) < 0)
+            {
+                return Search(node.Left, value);
+            }
+
+            return Search(node.Right, value);
+        }
+
+        public void Display()
+        {
+            Display(root);
+        }
+
+        private void Display(TreeNode<T> node)
+        {
+            if (node != null)
+            {
+                Display(node.Left);
+                System.Console.WriteLine(node.Value);
+                Display(node.Right);
+            }
+        }
+    }
+
+    public class TreeNode<T>
+    {
+        public T Value { get; set; }
+        public TreeNode<T> Left { get; set; }
+        public TreeNode<T> Right { get; set; }
+
+        public TreeNode(T value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+}
--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -4,21 +4,19 @@ namespace Exemple
 {
     internal class Program
     {
-        
+
 
         static void Main(string[] args)
         {
-            var testArray = new Test[]
-            {
-                new Test{Value = 1, ValueName = "one"},
-                new Test{Value = 2, ValueName = "two"},
-                new Test{Value = 3, ValueName = "thre"},
-                new Test{Value = 4, ValueName = "four"},
-                new Test{Value = 5, ValueName = "one"},
-            };
+            var intTree = new BinarySerachTeary<int>();
+            intTree.Insert(10);
+            intTree.Insert(5);
+            intTree.Insert(15);
+            intTree.Insert(3);
 
-            var item = Findeble.FindItem<Test>(testArray, x => x.ValueName == "one");
-            Console.WriteLine(item);
+            intTree.Display();
+
+            Console.WriteLine(intTree.Search(20));
         }
     }
 }
