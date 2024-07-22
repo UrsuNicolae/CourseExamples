7ead258606fb63acad6628647b08aa472fe93340
Nicolae URSU <nicolae1.ursu@orange.com>
2024-07-22 20:22:59 +0300

    Add Store class

---
+++ Exemple/Genercs/EntityRepository.cs
@@ -0,0 +1,110 @@
+﻿namespace Exemple.Genercs
+{
+    public interface IRepository<T, TKey> where T : class, new()
+        where TKey : struct
+    {
+        void Create(T entity);
+        void Read(TKey entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+    }
+
+
+    public class BaseEntity<TKey> where TKey : struct
+    {
+        public TKey Id { get; private set; }
+
+        public BaseEntity(TKey id)
+        {
+            Id = id;
+        }
+    }
+
+
+
+    public class EntityRepository<T, TKey> : IRepository<T, TKey>
+        where T : BaseEntity<TKey>, new()
+        where TKey : struct
+    {
+        private readonly Dictionary<TKey, T> _store;
+        public EntityRepository()
+        {
+            _store = new Dictionary<TKey, T>();
+        }
+        public void Create(T entity)
+        {
+            if (_store.ContainsKey(entity.Id))
+            {
+                throw new InvalidOperationException($"Entity with id {entity.Id} already exists");
+            }
+            _store.Add(entity.Id, entity);
+        }
+
+        public void Delete(T entity)
+        {
+            if (!_store.ContainsKey(entity.Id))
+            {
+                throw new InvalidOperationException($"Entity with id {entity.Id} already exists");
+            }
+            _store.Remove(entity.Id);
+        }
+
+        public void Read(TKey key)
+        {
+            if (!_store.ContainsKey(key))
+            {
+                throw new InvalidOperationException($"Entity with id {key} already exists");
+            }
+            var value = _store.GetValueOrDefault(key);
+            Console.WriteLine(value);
+        }
+
+        public void Update(T entity)
+        {
+            if (_store.ContainsKey(entity.Id))
+            {
+                _store[entity.Id] = entity;
+            }
+        }
+    }
+
+    public class StoreCustomer : BaseEntity<int>
+    {
+        public string Name { get; set; }
+
+        public StoreCustomer() : base(0)
+        {
+        }
+
+        public StoreCustomer(int id, string name) : base(id)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}";
+        }
+    };
+
+    public class StoreProduct<TKey> : BaseEntity<TKey> where TKey : struct
+    {
+        public string Name { get; set; }
+
+        public StoreProduct() : base(default)
+        {
+        }
+
+        public StoreProduct(TKey id, string name) : base(id)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}";
+        }
+    };
+}
--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -8,19 +8,15 @@ namespace Exemple
 
         static void Main(string[] args)
         {
-            var dictionary = new PhoneDictionary();
-            dictionary.Add("John", "123456789");
-            dictionary.Add("Nick", "123122222");
-            dictionary.Add("Tom", "123456789");
-            dictionary.Add("John", "123123");
+            var store = new EntityRepository<StoreCustomer, int>();
+            store.Create(new StoreCustomer(1, "John"));
+            store.Create(new StoreCustomer(2, "Doe"));
+            store.Read(1);
 
-
-            Console.WriteLine(dictionary);
-
-
-            dictionary.RemovePhone("Tom", "123456789");
-            dictionary.Remove("John");
-            Console.WriteLine(dictionary);
+            var store2 = new EntityRepository<StoreProduct<char>, char>();
+            store2.Create(new StoreProduct<char>('1', "Product 1"));
+            store2.Create(new StoreProduct<char>('2', "Product 2"));
+            store2.Read('2');
         }
     }
 }
