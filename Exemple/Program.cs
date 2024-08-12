1e6f9f010a55f6a4f3fb6715c79760f04f59a5b8
Nicolae URSU <nicolae1.ursu@orange.com>
2024-08-12 20:48:29 +0300

    Add postman collection

--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -4,15 +4,33 @@ namespace Exemple
 {
     internal class Program
     {
+        public static bool StartWithLetter(string srt, char letter)
+        {
+            return srt.StartsWith(letter);
+            //return srt[0] == letter;
+        }
 
         static void Main(string[] args)
         {
+            Predicate<string> startWith = x => x.Length > 3;
+            Func<string, bool> startWithFunc = x => x.Length > 3;
+            var test ="gello";
+
             var strings = new List<string>() { "hello", "world", "foo", "bar" };
-            StringFilterClass.StringFilter startWith = str => StringFilterClass.StartWithLetter(str, 'h');
+            var strings1 = strings.RemoveAll(x =>
+            {
+                var result = x.Length > 3;
+                if (result)
+                {
+                    Console.WriteLine(x);
+                }
+                return result;
+            });
+            /*StringFilterClass.StringFilter startWith = str => StringFilterClass.StartWithLetter(str, 'h');
             StringFilterClass.StringFilter contains = str => StringFilterClass.ContainsSubString(str, "o");
             StringFilterClass.StringFilter longerThan = str => StringFilterClass.IsLongerThan(str, 3);
 
-            var filterdStrings  = StringFilterClass.FilterStrings(strings, new List<StringFilterClass.StringFilter> { startWith, contains, longerThan});
+            var filterdStrings  = StringFilterClass.FilterStrings(strings, new List<StringFilterClass.StringFilter> { startWith, contains, longerThan});*/
         }
     }
 }
